e56f97c579a6c0c61f3447f819cb1d6b5da9f1fd
Vladimir <fominsky_v@live.com>
2022-06-28 00:56:49 +0500

    lesson_3

---
+++ src/main/java/ru/gb/shopspringjune22/Product.java
@@ -0,0 +1,16 @@
+package ru.gb.shopspringjune22;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    private Long id;
+    private String title;
+}
---
+++ src/main/java/ru/gb/shopspringjune22/ProductsController.java
@@ -0,0 +1,42 @@
+package ru.gb.shopspringjune22;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductsController {
+
+    private ProductsRepository productsRepository;
+
+    public ProductsController(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productsRepository.getItems();
+    }
+
+    @GetMapping("products/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productsRepository.getItems()
+                .stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+
+    @PostMapping("/products")
+    public void addNewProduct(@RequestBody Product product) {
+        productsRepository.add(product);
+    }
+
+    @DeleteMapping("/deleteAll")
+    public void deleteAll() {
+
+    }
+
+
+
+}
