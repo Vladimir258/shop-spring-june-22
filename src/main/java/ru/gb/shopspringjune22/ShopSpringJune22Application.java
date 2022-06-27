deba24a83a2887ab44da581d81e9fae4ee7188af
Vladimir <fominsky_v@live.com>
2022-06-28 00:17:39 +0500

    spring_io

---
+++ src/main/java/ru/gb/shopspringjune22/ShopSpringJune22Application.java
@@ -0,0 +1,13 @@
+package ru.gb.shopspringjune22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSpringJune22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSpringJune22Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/shopspringjune22/ShopSpringJune22ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.shopspringjune22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSpringJune22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
